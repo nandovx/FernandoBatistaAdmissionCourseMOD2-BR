6ce6ba81c97be1acd2a84c969daa2ef36f1cd4ca
Fernando Alexandre Batista Silva <nando.abs.18@gmail.com>
2022-10-25 00:23:37 -0300

    Função DUCK
    Incrementado a função Duck para o dino

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,8 +1,9 @@
 import pygame
-from dino_runner.utils.constants import JUMPING, RUNNING
+from dino_runner.utils.constants import JUMPING, RUNNING, DUCKING
 
 X_POS = 80
 Y_POS = 310
+Y_POS_DUCK = 338
 JUMP_VEL = 8.5
 
 class Dinosaur:
@@ -13,12 +14,16 @@ class Dinosaur:
         self.dino_rect.y = Y_POS
         self.step_index = 0
         self.dino_run = True
-        self.dino_jump = False
+        self.dino_duck = False
+        self.dino_jump = False        
         self.jump_vel = JUMP_VEL
 
     def update(self, user_input):
         if self.dino_jump:
             self.jump()
+        
+        if self.dino_duck:
+            self.duck()
 
         if self.dino_run:
             self.run()
@@ -26,12 +31,21 @@ class Dinosaur:
         if self.step_index >= 10:
             self.step_index = 0
 
-        if user_input[pygame.K_UP] and not self.dino_jump:
+        if user_input[pygame.K_DOWN] and not self.dino_duck and not self.dino_jump:
+            self.dino_duck = True
+            self.dino_run = False
+        elif not user_input[pygame.K_DOWN]:
+            self.dino_duck = False
+        
+        if not self.dino_jump and not user_input[pygame.K_DOWN]:
+            self.dino_run = True
+
+        if user_input[pygame.K_UP] and not self.dino_jump and not self.dino_duck:
             self.dino_jump = True
             self.dino_run = False
         elif not self.dino_jump:
             self.dino_jump = False
-            self.dino_run = True
+            
     
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
@@ -49,6 +63,11 @@ class Dinosaur:
             self.dino_rect.y = Y_POS
             self.dino_jump = False
             self.jump_vel = JUMP_VEL
+    
+    def duck(self):
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect.y = Y_POS_DUCK
+        self.step_index += 1
 
     def draw(self, screen: pygame.Surface):
         screen.blit(self.image,(self.dino_rect.x, self.dino_rect.y))
